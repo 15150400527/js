6105000ac4a38b6e042379bb9a0b887767e63789
zoe <31910818+zoewe@users.noreply.github.com>
2018-01-10 12:51:43 +0800

    修改
    修改

--- mode.js
+++ mode.js
@@ -15,7 +15,7 @@ BookManagerSystem.prototype.addBook=function(name,author,number,publishing,price
     for(var i=0;i<this.bookArr.length;i++){
         if(this.bookArr[i].name==name&&this.bookArr[i].author==author){
             alert("图书已经存在");
-           return false;
-        }
-    }
-}
+           break;
+		}
+		}
+	}
