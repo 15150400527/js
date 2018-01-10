80ff0f4a38f4c3d70295961aeb5f1674f3d5b023
zoe <31910818+zoewe@users.noreply.github.com>
2018-01-10 18:05:52 +0800

    登录、注册
    登录、注册功能的完善

--- mode.js
+++ mode.js
@@ -5,9 +5,19 @@ function Book(name,author,number,publishing,price){
 	this.publishing=publishing;
     this.price=price;
 }
+
+function User(user,password,email){
+	this.user=user;
+	this.password=password;
+	this.email=email;
+}
+
 Book.prototype.show=function(){
     console.log(this.name);
 }
 function BookManagerSystem(){
     this.bookArr=[];//用来存储Book类对象
+	 this.userArr=[];
 }
+
+
